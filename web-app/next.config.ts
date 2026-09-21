a14dcb64fbded78702c5360bd82ce53269b22644
Andrew Strasser <astrozine@gmail.com>
2026-09-21 04:12:41 -0300

    Remove next-intl from next config

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'heumjowshymszctoetlx.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
