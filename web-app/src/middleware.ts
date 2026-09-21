1f8f45f6da266f56ea6fd3196096fe46959fe3f8
Andrew Strasser <astrozine@gmail.com>
2026-09-21 02:46:52 -0300

    Fix i18n middleware matcher

--- web-app/src/middleware.ts
+++ web-app/src/middleware.ts
@@ -4,6 +4,6 @@ import {routing} from './i18n/routing';
 export default createMiddleware(routing);
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(pt|en|es|it|fr|de|nl)/:path*']
+  // Match all pathnames except for API, static files, next internals
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
